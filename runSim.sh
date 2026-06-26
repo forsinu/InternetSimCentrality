82d08f07e2942dd0f4360819c3bbc96d1ea6d9c9
forsinu <887708@stud.unive.it>
2026-06-26 11:11:04 +0200

    [SMALL-FIX] Add in runSim.sh config for SLURM

--- runSim.sh
+++ runSim.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+#SBATCH --job-name=InternetSim
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=80gb
+#SBATCH --time=24:00:00
+#SBATCH --partition=vhpc
+
 set -euo pipefail
 
 # Get the Absolute Path of the setup Script
